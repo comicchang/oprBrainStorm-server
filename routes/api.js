94d10e14f893c0d0c9f2dd17b15a0cb5e4ab6e84
Comic Chang <comicchang@gmail.com>
2017-12-20 14:31:17 +0800

    基本功能已经可用

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var oprRecord = require('../model.js');
+var async = require('async');
 
 /* GET */
-router.get('/', function (req, res, next) {
+router.get('/*select*%20B%3D\':imagedir\'*', function (req, res, next) {
 
-    res.render('index', {title: 'Express'});
+    console.log(req.params['imagedir']);
+
+    oprRecord.find({imageUrl:req.params['imagedir']}, function (err, docs) {
+        var onerow;
+        var result;
+
+        result = JSON.parse('{"table":{"cols":[{"label":"stars"},{"label":"author"},{"label":"duplicate"},{"label":"reasons"}],"rows":[]}}');
+        onerow = JSON.parse('{"c":[{"v":0},{"v":""},{"v":""},{"v":null},{"v":""}]}');//?
+
+        async.each (
+            docs,
+            function(doc, callback) {
+                onerow.c[0].v=doc.stars;
+                onerow.c[1].v=doc.author;
+                onerow.c[2].v=doc.duplicate?doc.duplicate:null;
+                onerow.c[3].v=doc.reasons;
+                result.table.rows.push (onerow);
+                callback ();
+            },
+            function (err) {
+                if (req.app.get('env') === 'development') {
+                    console.log(JSON.stringify(result));
+                }
+                res.render('api', {objects: result});
+            });
+    });
 });
 
 router.post('/', function (req, res) {
-    //console.log(req.body);
+    if (req.app.get('env') === 'development') {
+        console.log(req.body);
+    }
     var record = new oprRecord ();
     var keys=['title','imageUrl','description','streetAddress','lat','lng','author','stars','duplicate','reasons','JSON'];
     keys.forEach( function (key) {
         record[key]=req.body[key];
     });
-    record.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('meow');
-        }});
-    oprRecord.find({}, function (err, docs) {
-        console.log (docs);
-        res.status(200).json(docs).end();
-    });
+    record.save();
+    res.status(200).end();
 });
 
 module.exports = router;
